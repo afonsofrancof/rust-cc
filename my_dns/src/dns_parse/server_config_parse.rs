4b8b0c7eddecf37ab9d3dc18a2ce32c97b80c207
Tiago Sousa <tsousa@DESKTOP-EK93Q88.localdomain>
2022-11-12 18:35:36 +0000

    Started logging

--- my_dns/src/dns_parse/server_config_parse.rs
+++ my_dns/src/dns_parse/server_config_parse.rs
@@ -1,25 +1,36 @@
 use std::{collections::HashMap, fs::File, path::Path, io::Read};
 use regex::Regex;
 use crate::dns_structs::server_config::ServerConfig;
+use log::{info,error};
+
 pub fn get(file_path: String) -> Result<ServerConfig,&'static str>{
-    let mut file = match File::open(file_path) {
-        Err(_err) => return Err("GEY"),
-        Ok(file) => file,
+    
+    info!("Opening file");
+    let mut file = match File::open(file_path.to_owned()) {
+        Ok(file) => {
+            info!("File opened successfully");
+            file
+        },
+        Err(_err) => {
+            error!("Failed to open file {}",file_path);
+            return Err("Failed to open file") 
+        },
     };
 
     // String em memoria com o ficheiro para dar parse
     let mut read = String::new();
 
     match file.read_to_string(&mut read) {
-        Err(_err) => return Err("Couldn't Read to String"),
-        Ok(_) => (),
+        Ok(_) => info!("Conversion to string successful"),
+        Err(_err) => {
+            error!("Couldn't read to string");
+            return Err("Couldn't read to String")
+        },
     };
-
+    
+    info!("Capturing variables");
     let regex_variables =
         Regex::new(r"(?m)^([a-z.0-9-]+) (DB|SS|DD|LG|ST) (.*)").unwrap();
-
-    
-
     
     let mut domain_name: String = String::new();
     let mut domain_db: String = String::new();
@@ -28,10 +39,12 @@ pub fn get(file_path: String) -> Result<ServerConfig,&'static str>{
     let mut domain_log: String = String::new();
     let mut all_log: String = String::new();
     let mut st_db: String = String::new();
+    
+    info!("Starting variable parse");
 
     for cap in regex_variables.captures_iter(&read){
         match &cap[2]{
-            "DB" => domain_db = cap[3].to_string(),
+            "DB" => {domain_name = cap[1].to_string();domain_db = cap[3].to_string()},
             "SS" => domain_ss.push(cap[3].to_string()),
             "DD" => {server_dd.insert(cap[1].to_string(),cap[3].to_string());},
             "LG" => match &cap[1]{
@@ -43,8 +56,5 @@ pub fn get(file_path: String) -> Result<ServerConfig,&'static str>{
         } 
     }
 
-
     Ok(ServerConfig{domain_name,domain_db,domain_ss,server_dd,domain_log,all_log,st_db})
-    
-
 } 
