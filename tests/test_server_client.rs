e8ae1c1fd3600a48c7f46c2940ddecb88bc4147a
afonsofrancof <me@afonsofrancof.com>
2022-12-19 15:51:07 +0000

    created a test between the server and a client

---
+++ tests/test_server_client.rs
@@ -0,0 +1,45 @@
+extern crate rustcc;
+
+use my_dns::dns_structs::dns_message::{QueryType, DNSMessage, DNSQueryInfo, DNSMessageData, DNSEntry, DNSMessageHeaders};
+use rustcc::{client, server};
+use std::{thread, time::Duration, collections::HashMap};
+
+#[test]
+fn test_client_server() {
+    let server = thread::spawn(move || {
+        server::start_server("etc/test-example-com.conf".to_string(), 5454,true)
+    });
+    thread::sleep(Duration::new(1, 0));
+    let client = thread::spawn(move || {
+        client::start_client(
+            "www.example.com".to_string(),
+            vec![QueryType::A],
+            4,
+            "127.0.0.1:5454".to_string(),
+        )
+    });
+
+    match client.join() {
+        Ok(msg) => {
+            let dns_query_info = DNSQueryInfo{ name: "www.example.com".to_string() ,type_of_value: vec![QueryType::A] } ;
+            //Fill in response_values
+            let mut response_values = HashMap::new();
+            response_values.insert(QueryType::A, vec![DNSEntry{ name: "www.example.com.".to_string(), type_of_value: "A".to_string(), value: "10.3.3.1".to_string(), ttl: 86400, priority: Some(200)}]);
+            //Fill in authorities_values
+            let authorities_values = vec![DNSEntry{ name: "example.com.".to_string(), type_of_value: "NS".to_string(), value: "ns1.example.com.".to_string(), ttl:86400, priority: None }];
+            //Fill in extra_values
+            let extra_values = vec![DNSEntry{ name: "ns1.example.com.".to_string(), type_of_value: "A".to_string(), value: "10.2.2.2".to_string(), ttl: 86400, priority: None}];
+            //Create dns_message_data
+            let dns_message_data = DNSMessageData{ query_info: dns_query_info, response_values: Some(response_values) , authorities_values: Some(authorities_values), extra_values: Some(extra_values)} ;
+            //Create dns_message_header
+            let dns_message_header = DNSMessageHeaders{ message_id: msg.header.message_id, flags: 1, response_code: Some(0), number_of_values: Some(1), number_of_authorities: Some(1), number_of_extra_values: Some(1)};
+
+            let dns_message = DNSMessage{ header: dns_message_header, data: dns_message_data};
+            assert_eq!(msg, dns_message);
+        }
+        Err(_err) => {
+            panic!("Client join failed on test")
+        }
+    };
+    server.join().unwrap();
+}
