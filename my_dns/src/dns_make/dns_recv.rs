e64aa6cea65305a19c331933e87d8a942ee2afd3
afonsofrancof <me@afonsofrancof.com>
2022-12-28 18:36:55 +0000

    Added Error enums to return

--- my_dns/src/dns_make/dns_recv.rs
+++ my_dns/src/dns_make/dns_recv.rs
@@ -1,16 +1,21 @@
 use crate::dns_structs::dns_message::DNSMessage;
 use bincode;
-use std::net::{SocketAddr, UdpSocket};
+use std::{net::{SocketAddr, UdpSocket}, io};
 
-pub fn recv(incoming_socket: &UdpSocket) -> Result<(DNSMessage,SocketAddr),&'static str> {
+pub enum RecvError{
+    IOError(io::Error),
+    DeserializeError(bincode::Error)
+}
+
+pub fn recv(incoming_socket: &UdpSocket) -> Result<(DNSMessage,SocketAddr),RecvError> {
     let mut recv_buf = [0; 1000];
     let (_, src_addr) = match incoming_socket.recv_from(&mut recv_buf) {
         Ok(bytes_and_addr) => bytes_and_addr,
-        Err(..) => return Err("Could not recieve data on incoming socket.")
+        Err(err) => return Err(RecvError::IOError(err))
     };
     let dns_message: DNSMessage = match bincode::deserialize(&recv_buf){
         Ok(message) => message,
-        Err(err) => panic!("{err}")
+        Err(err) => return Err(RecvError::DeserializeError(err))
     };
     Ok((dns_message,src_addr))
 }
--- my_dns/src/dns_make/dns_send.rs
+++ my_dns/src/dns_make/dns_send.rs
@@ -1,17 +1,16 @@
 use crate::dns_structs::dns_message::*;
 use bincode;
-use std::net::{UdpSocket};
+use std::{io, net::UdpSocket};
 
 pub fn send(
     dns_message: DNSMessage,
-    socket :  &UdpSocket,
-    remote_addr_and_port : String,
-) -> Result<usize,std::io::Error> {
+    socket: &UdpSocket,
+    remote_addr_and_port: String,
+) -> Result<usize, std::io::Error> {
     //Serialize the DNSMessage to JSON (Temporarily)
     let dns_message_serialized = bincode::serialize(&dns_message).unwrap();
     let dns_message_bytes: &[u8] = &dns_message_serialized;
     //Send DNSMessage to the Dns Server
-    println!("IP: {}",remote_addr_and_port);
-    let bytes_sent = socket.send_to(dns_message_bytes,remote_addr_and_port.as_str());
-    bytes_sent
+    println!("IP: {}", remote_addr_and_port);
+    socket.send_to(dns_message_bytes, remote_addr_and_port.as_str())
 }
