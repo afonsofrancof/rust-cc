6cefc9127ca94358e66c866e1625d28760e893d4
afonsofrancof <me@afonsofrancof.com>
2023-01-02 00:11:48 +0000

    Fixed test for new return type of start_sr

--- tests/test_server_client.rs
+++ tests/test_server_client.rs
@@ -1,42 +1,93 @@
 extern crate rustcc;
 
-use my_dns::{dns_structs::{dns_message::{QueryType, DNSMessage, DNSQueryInfo, DNSMessageData, DNSEntry, DNSMessageHeaders}, dns_domain_name::Domain}, dns_parse::server_config_parse};
+use my_dns::{
+    dns_components::sr::start_sr,
+    dns_parse::server_config_parse,
+    dns_structs::{
+        dns_domain_name::Domain,
+        dns_message::{
+            DNSEntry, DNSMessage, DNSMessageData, DNSMessageHeaders, DNSQueryInfo, QueryType,
+        },
+    },
+};
 use rustcc::{client, server};
-use std::{thread, time::Duration, collections::HashMap};
+use std::{collections::HashMap, net::SocketAddr, thread, time::Duration};
 
 #[test]
 fn test_client_server() {
     let server = thread::spawn(move || {
         let config = server_config_parse::get("etc/test-example-com.conf".to_string()).unwrap();
-        server::start_server(config, 5454,true,true)
+        server::start_server(config, 5454, true, true)
     });
     thread::sleep(Duration::new(1, 0));
     let client = thread::spawn(move || {
-        client::start_client(
-            Domain::new("www.example.com".to_string()),
-            QueryType::A,
-            4,
-            "127.0.0.1:5454".to_string(),
+        let mut query =
+            client::query_builder(Domain::new("www.example.com".to_string()), QueryType::A, 4);
+        start_sr(
+            &mut query,
+            vec!["127.0.0.1:5454".to_string().parse().unwrap()],
+            true,
         )
     });
 
     match client.join() {
         Ok(msg) => {
-            let dns_query_info = DNSQueryInfo{ name: Domain::new("www.example.com".to_string()) ,type_of_value: QueryType::A } ;
-            //Fill in response_values
-            let response_values = vec![DNSEntry{ domain_name: Domain::new("www.example.com.".to_string()), type_of_value: "A".to_string(), value: "10.3.3.1".to_string(), ttl: 86400, priority: Some(200)}];
-            //Fill in authorities_values
-            let authorities_values = vec![DNSEntry{ domain_name: Domain::new("example.com.".to_string()), type_of_value: "NS".to_string(), value: "ns1.example.com.".to_string(), ttl:86400, priority: None }];
-            //Fill in extra_values
-            let extra_values = vec![DNSEntry{ domain_name: Domain::new("ns1.example.com.".to_string()), type_of_value: "A".to_string(), value: "10.2.2.2".to_string(), ttl: 86400, priority: None}];
-            //Create dns_message_data
-            let dns_message_data = DNSMessageData{ query_info: dns_query_info, response_values: Some(response_values) , authorities_values: Some(authorities_values), extra_values: Some(extra_values)} ;
-            //Create dns_message_header
-            let dns_message_header = DNSMessageHeaders{ message_id: msg.header.message_id, flags: 1, response_code: Some(0), number_of_values: Some(1), number_of_authorities: Some(1), number_of_extra_values: Some(1)};
+            match msg {
+                Ok(response) => {
+                    let dns_query_info = DNSQueryInfo {
+                        name: Domain::new("www.example.com".to_string()),
+                        type_of_value: QueryType::A,
+                    };
+                    //Fill in response_values
+                    let response_values = vec![DNSEntry {
+                        domain_name: Domain::new("www.example.com.".to_string()),
+                        type_of_value: "A".to_string(),
+                        value: "10.3.3.1".to_string(),
+                        ttl: 86400,
+                        priority: Some(200),
+                    }];
+                    //Fill in authorities_values
+                    let authorities_values = vec![DNSEntry {
+                        domain_name: Domain::new("example.com.".to_string()),
+                        type_of_value: "NS".to_string(),
+                        value: "ns1.example.com.".to_string(),
+                        ttl: 86400,
+                        priority: None,
+                    }];
+                    //Fill in extra_values
+                    let extra_values = vec![DNSEntry {
+                        domain_name: Domain::new("ns1.example.com.".to_string()),
+                        type_of_value: "A".to_string(),
+                        value: "10.2.2.2".to_string(),
+                        ttl: 86400,
+                        priority: None,
+                    }];
+                    //Create dns_message_data
+                    let dns_message_data = DNSMessageData {
+                        query_info: dns_query_info,
+                        response_values: Some(response_values),
+                        authorities_values: Some(authorities_values),
+                        extra_values: Some(extra_values),
+                    };
+                    //Create dns_message_header
+                    let dns_message_header = DNSMessageHeaders {
+                        message_id: response.header.message_id,
+                        flags: 1,
+                        response_code: Some(0),
+                        number_of_values: Some(1),
+                        number_of_authorities: Some(1),
+                        number_of_extra_values: Some(1),
+                    };
 
-            let dns_message = DNSMessage{ header: dns_message_header, data: dns_message_data};
-            assert_eq!(msg, dns_message);
-        }
+                    let dns_message = DNSMessage {
+                        header: dns_message_header,
+                        data: dns_message_data,
+                    };
+                    assert_eq!(response, dns_message);
+                }
+                Err(err) => panic!("{err}"),
+            }
+        },
         Err(_err) => {
             panic!("Client join failed on test")
         }
